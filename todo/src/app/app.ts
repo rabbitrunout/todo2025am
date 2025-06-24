9dd399dfc035a327c568a96ee7b7faa4a2c8243d
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-06-24 16:00:46 -0400

    app.html completed

--- todo/src/app/app.ts
+++ todo/src/app/app.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { TodoList } from './todoList';
 import { TodoItem } from './todoItem';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatBadgeModule } from '@angular/material/badge'; 
+import { MatTableModule  } from '@angular/material/table'; 
+import { MatCheckboxModule  } from '@angular/material/checkbox'; 
+import { MatFormFieldModule   } from '@angular/material/form-field'; 
+import { MatInputModule  } from '@angular/material/input'; 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  standalone: false,
+  standalone: true,
+  imports: [FormsModule,
+    MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule, MatTableModule, MatCheckboxModule , 
+    MatFormFieldModule, MatInputModule, MatSlideToggleModule 
+  ],
   styleUrl: './app.css'
 })
 export class App {
